a496552f9dbfacc4a68c3a01bc699ffeb599361d
Eduardo <eduardojose.campos@gmail.com>
2021-03-26 18:36:49 -0600

    Producto

--- frontend-proyecto-ingenieria/src/app/paginas/productos/productos.component.ts
+++ frontend-proyecto-ingenieria/src/app/paginas/productos/productos.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductoService } from 'src/app/servicios/producto.service';
 
-<<<<<<< HEAD
-
-=======
->>>>>>> b22e48c8e545297fcf8549d7b7e975162b9dcba8
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
